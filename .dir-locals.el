61ef18228a0149ff89cc4509b802725b6118e0db
Nick Henderson <525247+nwh@users.noreply.github.com>
2024-11-20 09:26:32 -0800

    add some environment files

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((python-mode
+  . ((eglot-workspace-configuration
+      . (:pylsp (:plugins (:jedi (:environment "/Users/nick/github.com/nwh/mlx-examples/.venv")
+                           :ruff (:enabled t))))))))
